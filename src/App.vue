9209e117078a50e3f06aebb3b80d164fe0587bb2
Darya Anton <foxik7477@gmail.com>
2024-06-14 01:06:56 +0500

    Компонентный подход.lesson02

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
-  <div id="app">
-    Hello Vue.js
-  </div>
+	<div class="wrapper">
+		<HeroView />
+		<FooterComponent />
+	</div>
 </template>
 
 <script>
+import HeroView from '@/views/HeroView.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
 
 export default {
-
+	components: {HeroView, FooterComponent}
 }
 </script>
 
