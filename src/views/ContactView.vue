614e89bc9797bfa2aada37316a31759a4bcf9edd
Darya Anton <foxik7477@gmail.com>
2024-06-18 04:28:44 +0500

    Обработчики событий v-on. Объект methods.lesson07

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -17,7 +17,7 @@
             <div class="title mt-5">Tell us about your tastes</div>
             <img class="beanslogo mt-5" src="@/assets/logo/Beans_logo_dark.svg" alt="Beans logo">
 
-            <form action="#" class="mt-5">
+            <form action="#" class="mt-5" @submit.prevent="submit">
               <div class="form-group row">
                 <div class="col col-12 col-sm-3 d-flex align-items-center">
                   <label for="name-input" class="mb-0">
@@ -26,7 +26,11 @@
                   </label>
                 </div>
                 <div class="col col-12 col-sm-9">
-                  <input type="text" class="form-control" id="name-input">
+                  <input 
+                    v-model="form.name" 
+                    type="text" 
+                    class="form-control" 
+                    id="name-input">
                 </div>
               </div>
 
@@ -38,7 +42,11 @@
                   </label>
                 </div>
                 <div class="col col-12 col-sm-9">
-                  <input type="email" class="form-control" id="email-input">
+                  <input 
+                    v-model="form.email" 
+                    type="email" 
+                    class="form-control" 
+                    id="email-input">
                 </div>
               </div>
 
@@ -49,7 +57,11 @@
                   </label>
                 </div>
                 <div class="col col-12 col-sm-9">
-                  <input type="tel" class="form-control" id="phone-input">
+                  <input 
+                    v-model="form.phone" 
+                    type="tel" 
+                    class="form-control" 
+                    id="phone-input">
                 </div>
               </div>
 
@@ -61,14 +73,19 @@
                   </label>
                 </div>
                 <div class="col col-12">
-                  <textarea class="form-control" name="message" id="message" rows="5"
+                  <textarea 
+                    v-model="form.text"
+                    class="form-control" 
+                    name="message" 
+                    id="message" 
+                    rows="5"
                     placeholder="Leave your comments here"></textarea>
                 </div>
               </div>
 
               <div class="row">
                 <div class="col">
-                  <button class="btn btn-outline-dark send-btn">Send us</button>
+                  <button type="submit" class="btn btn-outline-dark send-btn">Send us</button>
                 </div>
               </div>
             </form>
@@ -82,6 +99,21 @@
 <script>
 import NavBarComponent from '@/components/NavBarComponent.vue'
 export default {
-  components: { NavBarComponent }
+  components: { NavBarComponent },
+  data() {
+    return {
+      form: {
+        name: '',
+        phone: '',
+        email: '',
+        text: ''
+      }
+    }
+  },
+  methods: {
+    submit() {
+      console.log(document.querySelector('form'));
+    }
+  }
 }
 </script>
--- src/views/HeroView.vue
+++ src/views/HeroView.vue
@@ -15,13 +15,19 @@
               We makes every day full of energy and taste
             </div>
             <div class="preview__subtitle">Want to try our beans?</div>
-            <a href="./coffeepage.html" class="preview__btn">More</a>
+
+            <a 
+              href="./coffeepage.html" 
+              class="preview__btn"
+              @click.prevent="smoothScroll"
+              >More</a>
+
           </div>
         </div>
       </div>
     </div>
 
-    <section class="about">
+    <section class="about" id="about" ref="about">
       <div class="container">
         <div class="row">
           <div class="col-lg-6 offset-lg-3">
@@ -48,7 +54,7 @@
     </section>
     <section class="best">
       <div class="container">
-        <div class="title">Our best</div>
+        <div class="title" ref="ourBest">Our best</div>
         <div class="row">
           <div class="col-lg-10 offset-lg-1">
 
@@ -74,6 +80,7 @@
 import NavBarComponent from '@/components/NavBarComponent.vue'
 import CardBestComponent from '@/components/CardBestComponent.vue'
 import { v4 as uuidv4 } from 'uuid';
+import { scrollIntoView } from "seamless-scroll-polyfill";
 
 export default {
   components: { NavBarComponent, CardBestComponent },
@@ -99,6 +106,14 @@ export default {
           price: 6.99
         },
       ]
+    }
+  },
+  methods: {
+    smoothScroll() {
+      scrollIntoView(this.$refs.ourBest, { 
+        behavior: "smooth", 
+        block: "start"
+      });
     }
   }
 }
